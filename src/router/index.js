45adfa0c0e3b5e26cfea8d0004d617fb3ac267b8
Alril Ambanaga <alril.ambanaga@gmail.com>
2026-01-21 17:04:03 +0800

    add HomePage

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/homepage/HomeView.vue'),
+    },
+    ,
+  ],
 })
 
 export default router
---
+++ src/views/homepage/component/HeaderComponent.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="h-full bg-blue-400 p-2">asdf</div>
+</template>
+<script setup></script>
