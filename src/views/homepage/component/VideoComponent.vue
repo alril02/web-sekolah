b6edfa74e63062da5daa701eee14429eac410006
Alril Ambanaga <alril.ambanaga@gmail.com>
2026-02-01 01:09:51 +0800

    update source video component menjadi iframe dari youtube

--- src/views/homepage/component/VideoComponent.vue
+++ src/views/homepage/component/VideoComponent.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="w-screen h-screen overflow-hidden bg-black">
-    <video
-      class="w-full h-full object-cover"
-      autoplay
-      muted
-      loop
-      playsinline
-    >
-      <source src="https://www.w3schools.com/html/movie.mp4" type="video/mp4" />
-      <source src="https://www.w3schools.com/html/movie.mp4" type="video/ogg" />
-      Browser Anda tidak mendukung pemutar video.
-    </video>
+  <div class="w-screen overflow-hidden bg-black">
+    <div class="w-full aspect-video">
+      <iframe
+        class="w-full h-full"
+      src="https://www.youtube.com/embed/0ZimPigb9U4?autoplay=1&mute=1&loop=1&playlist=0ZimPigb9U4&controls=0&modestbranding=1&rel=0&playsinline=1"
+      title="Video Profil"
+      frameborder="0"
+      allow="autoplay; encrypted-media; picture-in-picture"
+      allowfullscreen
+      ></iframe>
+    </div>
   </div>
 </template>
