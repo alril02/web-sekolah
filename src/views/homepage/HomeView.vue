66e7eae14eca85b886d88482abc89869c5b00cbe
Alril Ambanaga <alril.ambanaga@gmail.com>
2026-02-01 01:11:22 +0800

    update Home View untuk menggunakan Header Component yang baru

--- src/views/homepage/HomeView.vue
+++ src/views/homepage/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
   <Header />
   <Video />
-  <Fitur />
+  <Fitur class="mt-[-10rem]" />
 
 </template>
 
 <script setup>
-import Header from './component/HeaderComponent.vue'
+import Header from './component/HeaderComponent2.vue'
 import Video from './component/VideoComponent.vue'
 import Fitur from './component/FiturUtamaComponent.vue'
 </script>
